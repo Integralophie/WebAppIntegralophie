8a1625e490d3ce39ed4f9760f4fa9ef1a9d03235
integralophie <sophiewu118@hotmail.com>
2019-04-25 13:55:09 -0400

    try to upload what I have

---
+++ main.js
@@ -0,0 +1,58 @@
+
+function myFunction() {
+  // use jQuery ($ is shorthand) to find the div on the page and then change the html
+  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+
+  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+
+  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+}
+  // Gets called whenever the user clicks "sign in" or "sign out".
+  function toggleSignIn() {
+    if (!firebase.auth().currentUser) { // if the user's not logged in, handle login
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/plus.login');
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        console.log("success");
+      }).catch(function(error) {
+        console.error("error", error);
+      });
+    } else { // handle logout
+      firebase.auth().signOut();
+    }
+    //This disables the button until login or logout is successful. You'll want to replace 'login-button' with the id of your login button.
+    $('#login-button').attr("disabled", true);
+  }
+
+function handleMessageFormSubmit() {
+  var firstn = $("#firstname").val();
+  var lastn = $("#lastname").val();
+  addMessage(lastn, firstn);
+}
+function addMessage(lastn,firstn){
+  var postData = {
+    lastn:lastn,
+    firstn:firstn,
+    title:title,
+    body:body
+  };
+  console.log(postData);
+  var newPostKey = firebase.database().ref().child('stream').push().key;
+  firebase.database().ref('/stream/' + newPostKey).set(postData);
+}
+window.onload = function() {
+  const databaseStreamReference = firebase.database().ref('/stream/');
+
+  databaseStreamReference.on('value', function(snapshot) {
+    var messages = snapshot.val();
+    $('#stream').empty();
+
+    if (messages) {
+      Object.keys(messages).forEach(function (key) {
+        const message = messages[key];
+        $('#stream').append(`<div>${message.firstn}</div>`);
+      });
+    }
+  });
+};
